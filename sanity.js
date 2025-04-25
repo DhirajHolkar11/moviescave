585c38cdea2e73555143168793146d500115d7ca
Dhiraj Holkar <dhirajspock2121@gmail.com>
2025-04-26 00:26:00 +0530

    fix css import path

--- sanity.js
+++ sanity.js
@@ -1,11 +1,8 @@
-
-
-// sanity.js
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.SANITY_PROJECT_ID,  // Use without NEXT_PUBLIC_ prefix
+  dataset: process.env.SANITY_DATASET,      // Use without NEXT_PUBLIC_ prefix
   useCdn: false,
   apiVersion: '2023-12-01',
 });
