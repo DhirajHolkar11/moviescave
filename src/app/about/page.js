b70b72e4424d29ae12d55ebcbd93726c68b563df
Dhiraj Holkar <dhirajspock2121@gmail.com>
2025-04-26 00:03:27 +0530

    fix css import path

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -10,7 +10,7 @@ const AboutPage = () => {
       <h1 className='heading'>About Us</h1>
       <p className='text'>
         Welcome to our website! We are passionate about sharing knowledge and content through our blog posts and movie collections. 
-        Our mission is to provide quality information and entertainment for our audience. Whether you're here to read insightful posts or explore movies, we're glad to have you!
+        Our mission is to provide quality information and entertainment for our audience. Whether you are here to read insightful posts or explore movies, we are glad to have you!
       </p>
       <p className='text'>
         Stay connected and explore more exciting content. If you have any questions or feedback, feel free to reach out to us anytime!
